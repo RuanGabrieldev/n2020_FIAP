5e573c3278ed2df5cda92ce29d81e997efeb6cab
Ana Beatriz Moreira de Oliveira <moreiranab@MacBook-Air-de-Ana-2.local>
2020-06-19 01:11:28 -0300

    recipe details screen

--- lib/screens/recipes_details_screen.dart
+++ lib/screens/recipes_details_screen.dart
@@ -3,163 +3,123 @@ import 'package:flutter/material.dart';
 import 'package:n2020/models/recipe_model.dart';
 
 class RecipesDetalhesScreen extends StatelessWidget {
-  RecipeModel recipeModel;
-
   @override
   Widget build(BuildContext context) {
-    recipeModel = ModalRoute.of(context).settings.arguments;
+    final RecipeModel recipeModel = ModalRoute.of(context).settings.arguments;
 
-    // final levelIndicator = Container(
-    //   child: Container(
-    //     child: ClipRRect(
-    //       borderRadius: BorderRadius.circular(4),
-    //       child: LinearProgressIndicator(
-    //         backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
-    //         value: recipeModel.rendimento.toDouble(),
-    //         valueColor: AlwaysStoppedAnimation(Colors.green),
-    //       ),
-    //     ),
-    //   ),
-    // );
-
-    final coursePrice = Container(
-      padding: const EdgeInsets.all(7.0),
-      decoration: new BoxDecoration(
-        border: new Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: Center(
-        child: new Text(
-          "Tempo " + recipeModel.tempoPreparo.toString(),
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-
-    final topContentText = Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        SizedBox(height: 80.0),
-        Icon(
-          Icons.computer,
-          color: Colors.white,
-          size: 40.0,
-        ),
-        Container(
-          width: 90.0,
-          child: new Divider(color: Colors.green),
-        ),
-        SizedBox(height: 10.0),
-        Text(
-          recipeModel.titulo,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 45.0,
-          ),
-        ),
-        SizedBox(height: 22.7),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            // Expanded(
-            //   flex: 1,
-            //   child: levelIndicator,
-            // ),
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: Text(
-                  recipeModel.titulo,
-                  style: TextStyle(color: Colors.white),
+    Column stringList(List<String> list) {
+      return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: list
+              .map(
+                (ing) => Text(
+                  ing,
+                  style: TextStyle(color: Colors.black54, fontSize: 16),
+                  textAlign: TextAlign.left,
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: coursePrice,
-            ),
-          ],
-        ),
-      ],
-    );
+              )
+              .toList());
+    }
 
-    final topContent = Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.only(left: 10.0),
-          height: MediaQuery.of(context).size.height * 0.5,
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: new AssetImage("drive.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.5,
-          padding: EdgeInsets.all(40.0),
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
-          child: Center(
-            child: topContentText,
-          ),
-        ),
-        Positioned(
-          left: 8.0,
-          top: 60.0,
-          child: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-          ),
-        )
-      ],
-    );
-
-    final bottomContentText = Text(
-      recipeModel.titulo,
-      style: TextStyle(fontSize: 18.0),
-      textAlign: TextAlign.justify,
-    );
-
-    final readButton = Container(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      width: MediaQuery.of(context).size.width,
-      child: RaisedButton(
-        onPressed: () => {},
-        color: Color.fromRGBO(58, 66, 86, 1.0),
+    Padding listTitle(String title) {
+      return Padding(
+        padding: EdgeInsets.all(20),
         child: Text(
-          "Tenho interesse!",
-          style: TextStyle(color: Colors.white),
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black87,
+          ),
         ),
-      ),
-    );
-
-    final bottomContent = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(40.0),
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            bottomContentText,
-            readButton,
-          ],
-        ),
-      ),
-    );
+      );
+    }
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          topContent,
-          bottomContent,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(recipeModel.titulo),
+              background: Image.network(
+                recipeModel.image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.alarm,
+                            size: 30,
+                            color: Colors.blue,
+                          ),
+                          Expanded(
+                            child: Text(
+                              " Tempo de preparo: " + recipeModel.tempoPreparo,
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.local_dining,
+                            size: 30,
+                            color: Colors.deepOrange,
+                          ),
+                          Expanded(
+                            child: Text(
+                              " Rendimento: " + recipeModel.rendimento,
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                listTitle("Ingredientes"),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: stringList(recipeModel.ingredientes),
+                ),
+                listTitle("Modo de preparo"),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: stringList(recipeModel.modoPreparo),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                ),
+              ],
+            ),
+          )
         ],
       ),
+      // Column(
+      //   children: <Widget>[
+      //     topContent,
+      //     bottomContent,
+      //   ],
+      // ),
     );
   }
 }
